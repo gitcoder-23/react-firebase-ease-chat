a335492769cc605c208265d9ff35328e11c9ad32
gitcoder-23 <prabhatece21@gmail.com>
2021-03-07 20:03:50 +0530

    Login  & signup ready to dashboard

--- src/login/login.js
+++ src/login/login.js
@@ -1,13 +1,106 @@
-import React from 'react'
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './styles';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import Paper from '@material-ui/core/Paper';
+import withStyles from '@material-ui/core/styles/withStyles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+// for v9.0
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 class LoginComponent extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            email: null,
+            password: null,
+            // loginError: 'fdfdfd',
+            loginError: '',
+        };
+    };
+
+    submitLogin = (e) => {
+        // page refresh block
+        e.preventDefault();
+        // console.log('Submitting!');
+
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(this.state.email, this.state.password)
+            .then(() => {
+                this.props.history.push('/dashboard');
+
+            }, err => {
+                this.setState({ loginError: 'Server error' });
+                console.log(err);
+            });
+    }
+
+    userTyping = (type, e) => {
+        // console.log(type, e);
+
+        switch (type) {
+            case 'email':
+            this.setState({email: e.target.value});
+            break;
+
+            case 'password':
+            this.setState({password: e.target.value});
+            break;
+
+            default:
+            break;
+
+        }
+    };
+
     render() {
+        const { classes } = this.props;
         return (
-            <div>
-                Hello World Login
-            </div>
+            <main className={classes.main}>
+                <CssBaseline></CssBaseline>
+                <Paper className={classes.paper}>
+                    <Typography component='h1' variant='h5'>
+                        Log In!
+                    </Typography>
+
+                    <form className={classes.form} onSubmit={(e) => this.submitLogin(e)}>
+                        <FormControl required fullWidth margin='normal'>
+                            <InputLabel htmlFor='login-email-input'>
+                                Enter Your Email
+                            </InputLabel>
+                            <Input autoComplete='email' autoFocus id='login-email-input' onChange={(e) => this.userTyping('email', e)}></Input>
+                        </FormControl>
+
+                        <FormControl required fullWidth margin='normal'>
+                            <InputLabel htmlFor='login-password-input'>
+                                Enter Your Password
+                            </InputLabel>
+                            <Input type='password'   id='login-password-input' onChange={(e) => this.userTyping('password', e)}></Input>
+                        </FormControl>
+
+                        <Button type="submit" fullWidth variant='contained' color='primary' className={classes.submit}>Submit</Button>
+
+                    </form>
+                    {
+                        this.state.loginError ? <Typography className={classes.errorText} component='h5' variant='h6'>
+                            Incorrect Login Information!
+                        </Typography> : null
+                    }
+                    <Typography component='h5' variant='h6' className={classes.noAccountHeader}>Don't have an account?</Typography>
+                    <Link to='/signup' className={classes.signUpLink}>Sign Up!</Link>
+                </Paper>
+            </main>
         )
     }
 };
 
-export default LoginComponent;
+export default withStyles(styles)(LoginComponent);
